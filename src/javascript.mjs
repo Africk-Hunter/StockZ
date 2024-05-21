b2b4b619a2feb72d6d6d2044384b46a207248e52
Africk-Hunter <hunterafrick@gmail.com>
2024-05-20 19:24:52 -0700

    Watch List Loading Change
    - The watchlist used to do full calculations on every stock in it every time it was loaded. I changed it so that when a user logs in all their data from their watchlist is pulled from the database and loaded into local storage. The watchlist now pulls from this local storage when loading to improve load times.
    - The refresh button will now run the calculations on all the stocks in the watchlist and refresh the data as well as updating it in the database.

--- src/javascript.mjs
+++ src/javascript.mjs
@@ -1,6 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js'
 import { getAuth, signInWithEmailAndPassword, signOut } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js'
-import { getFirestore, collection, setDoc, getDocs, doc, deleteDoc } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js'
+import { getFirestore, collection, setDoc, getDocs, doc, deleteDoc, updateDoc } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js'
 
 const firebaseConfig = {
   apiKey: "AIzaSyAuxROpJhqJ4-fgIC4xwNYV5ycd0O_QCO4",
@@ -19,6 +19,7 @@ const auth = getAuth(app);
 const MAX_VALUE             = 9999;
 let currentTicker           = "";
 /* Elements */
+let stockZLogo              = document.getElementById("stockZLogo");
 var enterButton             = document.getElementById("enterButton");
 let logOutButton            = document.getElementById("logOutButton");
 let inputUsername           = document.getElementById('inputUsername');
@@ -36,6 +37,8 @@ let tickerLabelIP           = document.getElementById("tickerLabelIP");
 let stockStatsLink          = document.getElementById("stockStatsLink");
 let stockDescLink           = document.getElementById("stockDescLink");
 let addToWatchlist          = document.getElementById("addToWatchlist");
+let grBLonPage              = document.getElementById("grBL");
+let bBHonPage               = document.getElementById("bBH");
 /* Watch List Page */
 let watchlistItemsContainer = document.getElementById("watchlistItemsContainer");
 let refreshButton           = document.getElementById("refreshButton");
@@ -49,7 +52,6 @@ auth.onAuthStateChanged((user) => {
     }
 });
 
-
 async function loginUser(username, password) {
     try {
       const userCredential = await signInWithEmailAndPassword(auth, username, password);
@@ -60,15 +62,14 @@ async function loginUser(username, password) {
       return null;
     }
   }
-  function handleLogin() {
+  async function handleLogin() {
     const username = inputUsername.value;
     const password = inputPassword.value;
 
-    loginUser(username, password).then((user) => {
-    if (username == "") {
+    if (username === "") {
         inputUsername.style.background = "red";
     }
-    if (password == "") {
+    if (password === "") {
         inputPassword.style.background = "red";
     }
     inputPassword.addEventListener('click', function () {
@@ -78,51 +79,65 @@ async function loginUser(username, password) {
         inputUsername.style.background = '#E4ECE4';
     });
 
-    if (user) {
-        document.getElementById("stockZ").classList.add("fadeAway");
-        var inputs = document.querySelectorAll('input[type="text"], input[type="password"]');
-        inputs.forEach(function (input) {
-            input.classList.add("fadeAway");
-        });
-        enterButton.classList.add("fadeAway");
-        setTimeout(() => {
-            window.location.href = '/main';
-        }, 200);
+    try {
+        const user = await loginUser(username, password);
+        if (user) {
+            document.getElementById("stockZ").classList.add("fadeAway");
+            var inputs = document.querySelectorAll('input[type="text"], input[type="password"]');
+            inputs.forEach(function (input) {
+                input.classList.add("fadeAway");
+            });
+            enterButton.classList.add("fadeAway");
+
+            let userWatchListData = await fetchWatchlistItems(user.uid);
+            localStorage.setItem('userWatchListData', JSON.stringify(userWatchListData));
+            setTimeout(() => {
+                window.location.href = '/main';
+            }, 200);
+        }
+    } catch (error) {
+        console.error("Error during login or fetching watchlist: ", error);
     }
-    });
-  }
+}
+
   
-  if (enterButton) {
+
+if (enterButton) {
     enterButton.addEventListener("click", handleLogin);
   
     inputUsername.addEventListener("keypress", function (event) {
-      if (event.key === "Enter") {
-        handleLogin();
-      }
+        if (event.key === "Enter") {
+            handleLogin();
+        }
     });
   
     inputPassword.addEventListener("keypress", function (event) {
-      if (event.key === "Enter") {
-        handleLogin();
-      }
+        if (event.key === "Enter") {
+            handleLogin();
+        }
     });
   } else {
+    stockZLogo.addEventListener('click', function(){
+        if (!document.URL.includes('main.html')) {
+            window.location.href = '/main';
+        }
+    })
     tickerInfoBtn.addEventListener('click', function () {
-      if (!document.URL.includes('tickerInfo.html')) {
-        window.location.href = '/tickerInfo';
-      }
+        if (!document.URL.includes('tickerInfo.html')) {
+            window.location.href = '/tickerInfo';
+        }
     });
     homeBtn.addEventListener('click', function () {
-      if (!document.URL.includes('main.html')) {
-        window.location.href = '/main';
-      }
+        if (!document.URL.includes('main.html')) {
+            window.location.href = '/main';
+        }
     });
     watchListBtn.addEventListener('click', function () {
-      if (!document.URL.includes('watchlist.html')) {
-        window.location.href = '/watchlist';
-      }
+        if (!document.URL.includes('watchlist.html')) {
+            window.location.href = '/watchlist';
+        }
     });
-  }
+}
 
 if (logOutButton) {
     logOutButton.addEventListener('click', function() {
@@ -204,7 +219,6 @@ if (tickerLabelIP) {
         console.log("Error occurred when loading data.");
     }
 
-
     stockStatsLink.addEventListener('click', function() {
         var url = 'https://finance.yahoo.com/quote/' + ticker + '/';
         window.open(url, '_blank');
@@ -213,7 +227,7 @@ if (tickerLabelIP) {
         var url = 'https://finance.yahoo.com/quote/' + ticker + '/profile';
         window.open(url, '_blank');
     });
-    tickerLabelIP.addEventListener('click', function(){
+    tickerLabelIP.addEventListener('click', function() {
         document.getElementById('buyHolder').style.display = 'none';
         document.getElementById('stockChart').style.display = 'none';
         document.getElementById('otherButtons').style.display = 'none';
@@ -222,21 +236,35 @@ if (tickerLabelIP) {
         setTimeout(() => {
             window.location.href = ('/main'); 
         }, 500);
-    })
-    addToWatchlist.addEventListener('click', function(){
+    });
+    addToWatchlist.addEventListener('click', function() {
         const user = auth.currentUser;
         if (user) {
-            addToWatchlistFunc(tickerLabelIP.innerHTML.toUpperCase(), user.uid);
-          } else {
-            console.error("No user is signed in. " + error);
-          }
-    })
+            addToWatchlistFunc(tickerLabelIP.innerHTML.toUpperCase(), grBLonPage.innerHTML, bBHonPage.innerHTML, user.uid);
+        } else {
+            console.error("No user is signed in.");
+        }
+    });
 }
 
-async function addToWatchlistFunc(ticker, uid) {
+async function addToWatchlistFunc(ticker, goodBuyPrice, badBuyPrice, uid) {
     const userDocRef = doc(db, `users/${uid}/watchlist`, ticker);
     try {
-        await setDoc(userDocRef, { ticker: ticker });
+        await setDoc(userDocRef, {
+            ticker: ticker,
+            goodBuyPrice: goodBuyPrice,
+            badBuyPrice: badBuyPrice
+        });
+        
+        let watchListItems = localStorage.getItem('userWatchListData');
+        let parsedWatchList = watchListItems ? JSON.parse(watchListItems) : [];
+        parsedWatchList.push({
+            ticker: ticker,
+            goodBuyPrice: goodBuyPrice,
+            badBuyPrice: badBuyPrice
+        });
+        localStorage.setItem('userWatchListData', JSON.stringify(parsedWatchList));
+
     } catch (e) {
         console.error("Error adding document: ", e);
     }
@@ -247,16 +275,49 @@ if(watchlistItemsContainer){
     refreshButton.addEventListener('click', async function(){
         const user = auth.currentUser;
         if (user) {
-            runWatchlist(user);
+            updateWatchListValues(user);
         } else {
             console.error("No user is signed in.");
         }
     });
 }
 
-async function runWatchlist(user){
+async function updateWatchListValues(user) {
     const watchlistItems = await fetchWatchlistItems(user.uid);
-    updateWatchlistUI(watchlistItems);
+
+    const updatedWatchlistItems = await Promise.all(watchlistItems.map(async (item) => {
+        await getStockData(item.ticker, 'mostRecentData');
+        var storageItem = localStorage.getItem('mostRecentData');
+        var data = JSON.parse(storageItem);
+        runStockCalculations(data, item.ticker, 'mostRecentCalculations');
+
+        const localCalculations = localStorage.getItem('mostRecentCalculations');
+        const calculations = JSON.parse(localCalculations);
+        const goodBuyPrice = calculations.goodBRLow.toFixed(2);
+        const badBuyPrice = calculations.badBRHigh.toFixed(2);
+
+        const userDocRef = doc(db, `users/${user.uid}/watchlist`, item.ticker);
+        await updateDoc(userDocRef, {
+            goodBuyPrice: goodBuyPrice,
+            badBuyPrice: badBuyPrice
+        });
+
+        return {
+            ticker: item.ticker,
+            goodBuyPrice: goodBuyPrice,
+            badBuyPrice: badBuyPrice
+        };
+    }));
+
+    localStorage.setItem('userWatchListData', JSON.stringify(updatedWatchlistItems));
+    runWatchlist(user);
+}
+
+
+async function runWatchlist(user) {
+    var watchListItems = localStorage.getItem('userWatchListData');
+    var parsedWatchList = JSON.parse(watchListItems);
+    updateWatchlistUI(parsedWatchList);
 }
 
 async function fetchWatchlistItems(uid) {
@@ -265,7 +326,12 @@ async function fetchWatchlistItems(uid) {
         const querySnapshot = await getDocs(watchlistCollectionRef);
         let watchlistItems = [];
         querySnapshot.forEach((doc) => {
-            watchlistItems.push(doc.id);
+            let data = doc.data();
+            watchlistItems.push({
+                ticker: doc.id,
+                goodBuyPrice: data.goodBuyPrice,
+                badBuyPrice: data.badBuyPrice
+            });
         });
         return watchlistItems;
     } catch (e) {
@@ -274,19 +340,17 @@ async function fetchWatchlistItems(uid) {
     }
 }
 
-function updateWatchlistUI(tickers) {
+function updateWatchlistUI(watchListItems) {
     const watchlistItemsContainer = document.getElementById('watchlistItemsContainer');
     watchlistItemsContainer.innerHTML = '';
 
-    tickers.forEach(ticker => {
-        const stockContainer = createStockContainerItem(ticker);
+    watchListItems.forEach(item => {
+        const stockContainer = createStockContainerItem(item);
         watchlistItemsContainer.appendChild(stockContainer);
     });
 }
 
-function createStockContainerItem(ticker) {
-    let gbPrice, bbPrice;
-
+function createStockContainerItem(item) {
     const container = document.createElement('div');
     container.className = 'stock-container';
 
@@ -295,111 +359,93 @@ function createStockContainerItem(ticker) {
 
     const nameDiv = document.createElement('div');
     nameDiv.className = 'flex h-full w-1/3 justify-center items-center bg-text-color rounded-l border-r select-none hover:cursor-pointer border-background transition-all duration-150 ease-in-out';
-    nameDiv.textContent = ticker;
+    nameDiv.textContent = item.ticker;
 
     const gbPriceDiv = document.createElement('div');
     gbPriceDiv.className = 'flex h-full w-1/3 justify-center items-center bg-great-buy-one border-r select-none border-background transition-all duration-150 ease-in-out';
+    gbPriceDiv.textContent = item.goodBuyPrice;
 
     const bbPriceDiv = document.createElement('div');
     bbPriceDiv.className = 'flex h-full w-1/3 justify-center items-center bg-desperate-buy-one rounded-r select-none transition-all duration-150 ease-in-out';
+    bbPriceDiv.textContent = item.badBuyPrice;
 
     const deleteIcon = document.createElement('button');
     deleteIcon.className = 'delete-icon flex h-full w-10 justify-center items-center bg-none rounded border-solid border border-text-color border-opacity-25 ml-auto hover:bg-text-color';
     deleteIcon.id = "watchlistDeleteButton";
     deleteIcon.innerHTML = '<img class="h-full w-4/5" src="../src/trashcan.svg" alt="Delete">';
-    deleteIcon.style.display = 'none'; 
+    deleteIcon.style.display = 'none';
 
     nameDiv.addEventListener('transitionend', handleTransitionEnd);
     gbPriceDiv.addEventListener('transitionend', handleTransitionEnd);
     bbPriceDiv.addEventListener('transitionend', handleTransitionEnd);
-    
-    stockItem.addEventListener('mouseenter', function() {
+
+    stockItem.addEventListener('mouseenter', function () {
         shrinkWatchlistItem();
     });
 
-    stockItem.addEventListener('mouseleave', function() {
+    stockItem.addEventListener('mouseleave', function () {
         deleteIcon.style.display = 'none';
-        unshrinkWatchlistItem()
+        unshrinkWatchlistItem();
     });
 
-    function shrinkWatchlistItem(){
+    function shrinkWatchlistItem() {
         nameDiv.classList.add("shrinkWaitlistBox");
         gbPriceDiv.classList.add("shrinkWaitlistBox");
         bbPriceDiv.classList.add("shrinkWaitlistBox");
     }
-    function unshrinkWatchlistItem(){
+    function unshrinkWatchlistItem() {
         nameDiv.classList.remove("shrinkWaitlistBox");
         gbPriceDiv.classList.remove("shrinkWaitlistBox");
         bbPriceDiv.classList.remove("shrinkWaitlistBox");
     }
-    
+
     function handleTransitionEnd() {
         if (checkIfClassIsApplied()) {
             deleteIcon.style.display = 'flex';
         }
     }
-    function checkIfClassIsApplied(){
+    function checkIfClassIsApplied() {
         return (
             nameDiv.classList.contains("shrinkWaitlistBox") &&
             gbPriceDiv.classList.contains("shrinkWaitlistBox") &&
             bbPriceDiv.classList.contains("shrinkWaitlistBox")
-        )
+        );
     }
-    
-    nameDiv.addEventListener('click', function(){
+
+    nameDiv.addEventListener('click', function () {
         watchListContainerLarge.classList.add("fadeAway");
         getStockData(nameDiv.textContent, 'mostRecentData')
-        .then(() => {
-            var storageItem = localStorage.getItem('mostRecentData');
-            var data = JSON.parse(storageItem);
-            runStockCalculations(data, nameDiv.textContent, 'mostRecentCalculations');
-            window.location.href = ('/tickerInfo'); 
-        })
-        .catch(error => {
-            console.error('Error occurred when retriving stock data: ', error);
-        });
-    })
-
-    deleteIcon.addEventListener('click', function(){
-        deleteFromFirebase(nameDiv.textContent);
-        stockItem.remove();
+            .then(() => {
+                var storageItem = localStorage.getItem('mostRecentData');
+                var data = JSON.parse(storageItem);
+                runStockCalculations(data, nameDiv.textContent, 'mostRecentCalculations');
+                window.location.href = ('/tickerInfo');
+            })
+            .catch(error => {
+                console.error('Error occurred when retrieving stock data: ', error);
+            });
     });
 
-    getStockData(ticker, 'watchListData')
-        .then(() => {
-            var watchListData = localStorage.getItem('watchListData');
-            var data = JSON.parse(watchListData);
-            runStockCalculations(data, ticker, 'watchListCalculations');
-            var localCalculations = localStorage.getItem('watchListCalculations');
+    deleteIcon.addEventListener('click', function () {
+        deleteFromFirebase(nameDiv.textContent);
+        stockItem.remove();
 
-            try {
-                if (localCalculations) {
-                    var calculations = JSON.parse(localCalculations);
-                    gbPrice = calculations.greatBRLow.toFixed(2);
-                    bbPrice = calculations.badBRHigh.toFixed(2);
-                    gbPriceDiv.textContent = gbPrice;
-                    bbPriceDiv.textContent = bbPrice;
-                    stockItem.appendChild(nameDiv);
-                    stockItem.appendChild(gbPriceDiv);
-                    stockItem.appendChild(bbPriceDiv);
-                    stockItem.appendChild(deleteIcon);
-            
-                    container.appendChild(stockItem);
-                } else {
-                    console.log("No calculations found in localStorage.");
-                }
-            } catch (error) {
-                console.log("Error occurred when loading data onto page.", error);
-            }
-        })
-        .catch(error => {
-            console.error('Error occurred when retrieving stock data: ', error);
-        });
+        var watchListItems = localStorage.getItem('userWatchListData');
+        var parsedWatchList = JSON.parse(watchListItems);
+        var updatedWatchList = parsedWatchList.filter(watchListItem => watchListItem.ticker !== item.ticker);
+        localStorage.setItem('userWatchListData', JSON.stringify(updatedWatchList));
+    });
 
-
+    stockItem.appendChild(nameDiv);
+    stockItem.appendChild(gbPriceDiv);
+    stockItem.appendChild(bbPriceDiv);
+    stockItem.appendChild(deleteIcon);
+    container.appendChild(stockItem);
 
     return container;
 }
+
+
 
 async function deleteFromFirebase(ticker) {
     const user = auth.currentUser;
@@ -419,7 +465,6 @@ async function deleteFromFirebase(ticker) {
         console.error("No user is signed in.");
     }
 }
-
 
 function getStockData(ticker, localStorageItem){
     return new Promise((resolve, reject) => {
@@ -472,7 +517,6 @@ function runStockCalculations(data, ticker, localStorageItem) {
         badBRLow: badBRLow,
         badBRHigh: badBRHigh
     };
-    console.log(localStorageItem);
     localStorage.setItem(localStorageItem, JSON.stringify(calculations));
 }
 
