d9576df0a27aabd7fb5077367714e633d8fe7493
Africk-Hunter <hunterafrick@gmail.com>
2024-11-03 16:47:21 -0800

    Header fix
    - Information wasn't populating correctly. Website headers for request needed to be updated.

--- app.py
+++ app.py
@@ -31,13 +31,19 @@ def run_calculations():
         print(url)
         
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Referer': 'https://finance.yahoo.com/',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
         }
+
         response = requests.get(url, headers=headers)
 
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'html.parser')
-            historical_table = soup.find('table', class_='table yf-ewueuo')
+            historical_table = soup.find('table', class_='table yf-h2urb6')
 
             if historical_table:
                 rows = historical_table.find_all('tr')
